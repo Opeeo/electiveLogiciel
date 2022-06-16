ab56af55db850559af86db4ffdaabb9f59747ec4
Clément Albos <albosclement@gmail.com>
2022-06-16 21:49:53 +0200

    Adding a navbar and MaterialUI

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/navbar';
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -12,7 +12,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header>
+        <Navbar />
+      </header>
+
       <main className={styles.main}>
+
+        
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
