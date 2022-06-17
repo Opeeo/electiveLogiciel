3e6a8994e24c0878deb4a9c463bc7bfb24477346
Philippe WOODCOCK <philippe.woodcock@viacesi.fr>
2022-06-17 14:11:03 +0200

    Implementing first test route

--- Restaurant/backend/server.js
+++ Restaurant/backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config;
+//const  { DB_URI } = require("./config/index");
+const {errorHandler} = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+//connectDB();
 
 const app = express();
 
@@ -8,5 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
 app.use('/api/', require('./routes/routes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
