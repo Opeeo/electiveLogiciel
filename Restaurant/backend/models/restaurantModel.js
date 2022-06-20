9434cceba71eb58e82a7343560fd290271677f8e
Philippe-Woodcock <58932863+phil-wdk@users.noreply.github.com>
2022-06-20 10:23:02 +0200

    Restaurant CRUD implemented

--- Restaurant/backend/models/restaurantModel.js
+++ Restaurant/backend/models/restaurantModel.js
@@ -1,40 +1,17 @@
 const mongoose = require('mongoose');
-const { getOriginalNode } = require('typescript');
 
 const restaurantSchema = mongoose.Schema({
-    name: {
-        String,
-        required: [true, 'Please add a name of the restaurant'],
-    },
-    siret_number: {
-        Number,
-        required: [true, 'Please add the Siret number of the restaurant'],
-    },
-    description: {
-        String,
-        required: [true, 'Please add a description of the restaurant'],
-    },
-    street_name: {
-        String,
-        required: [true, 'Please add the street of the restaurant'],
-    },
+    name: String,
+    siret_number: Number,
+    description: String,
+    street_name: String,
     additional_adress: String,
-    street_number: {
-        Number,
-        required: [true, 'Please add the street number of the restaurant'],
-    },
-    postal_code: {
-        Number,
-        required: [true, 'Please add the postal code of the restaurant'],
-    },
-    city: {
-        String,
-        required: [true, 'Please add the city of the restaurant'],
-    },
-    id_restaurator: {
-        Number,
-        required: [true, 'Please add the id of the owner of the restaurant'],
-    },
+    street_number: Number,
+    postal_code: Number,
+    city: String,
+    id_restaurator: Number,
+},{
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
--- Restaurant/backend/routes/routes.js
+++ Restaurant/backend/routes/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getRestaurants } = require('../controller/controller');
+const { getRestaurants, getARestaurant, createARestaurant, updateARestaurant, deleteARestaurant } = require('../controller/controller');
 
-router.get('/restaurants/', getRestaurants);
+router.get('/restaurant/', getRestaurants);
+
+router.get('/restaurant/:id', getARestaurant);
+
+router.post('/restaurant/', createARestaurant);
+
+router.put('/restaurant/:id', updateARestaurant);
+
+router.delete('/restaurant/:id', deleteARestaurant);
 
 module.exports = router;
