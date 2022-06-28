ea4f69b1a674d22709a0744107ca308f50c8fcd8
Philippe-Woodcock <58932863+phil-wdk@users.noreply.github.com>
2022-06-28 09:38:42 +0200

    CND implemented with return of an image

--- CND/server.js
+++ CND/server.js
@@ -4,6 +4,7 @@ const file = require("./model/model");
 const asyncHandler = require ("express-async-handler");
 const connectDB = require("./config/db");
 
+
 connectDB(); 
 
 const app = express();
@@ -24,10 +25,11 @@ app.get('/test', (req, res, next) => {
 });
 
 app.post('/api/uploadFile', upload.single('image'), asyncHandler(async (req,res) => {
+
     try{
         const newFile = await file.create({
             name: req.file.filename
-        })
+        });
         console.log(req.file);
         res.status(200).json(`File uploaded ${newFile}`);
     }catch(error){
@@ -39,7 +41,7 @@ app.post('/api/uploadFile', upload.single('image'), asyncHandler(async (req,res)
 app.get('/api/getFile/:id', asyncHandler(async (req, res) => {
     const getfile = await file.findOne({_id : req.params.id});
 
-    res.status(200).json(getfile);
+    await res.sendFile((`./images/${getfile.name}`), {root: './'});
 }));
 
 app.listen(8000);
