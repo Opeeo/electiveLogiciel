408408da32b85a987466a60b117d2bd918d364e3
Philippe-Woodcock <58932863+phil-wdk@users.noreply.github.com>
2022-06-30 10:57:01 +0200

    Revert "restores implementation"
    This reverts commit b455bc4eac1821641829e7cf2b0f2a32ff362ab0.

--- frontend/pages/restorers/home.tsx
+++ frontend/pages/restorers/home.tsx
@@ -1,21 +1,14 @@
 //Home page for restaurer
 import { NextPage } from "next";
-import Grid from '@mui/material/Grid';
-import Item from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import styles from "../../styles/Home.module.css";
 import Layout from "../../components/layout";
-import Dashboard from '../../components/dashboard';
 
 
 const Home: NextPage = () => {
     return (
         <Layout authentified={true} home={true}>
-            <Dashboard>
             <div>
-                <h1>For merchants</h1>
+                <h1>Home</h1>
             </div>
-            </Dashboard>            
         </Layout>
 
     );
